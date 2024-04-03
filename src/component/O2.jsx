29ce43ab158145f8b1c576f5d80d379a571beec2
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-03 22:27:45 +0100

    yyy

--- src/component/O2.jsx
+++ src/component/O2.jsx
@@ -11,6 +11,17 @@ const O2 = () => {
 <div className="kwa">Research</div>
   </div>
 </div>
+<div className="na-txt1">
+  <p className='small-txt2'>
+  We’re teaching AI to understand and simulate the physical world in motion, with the goal of training models that help people solve problems that require real-world interaction.
+<br></br>
+<br></br>
+Introducing Sora, our text-to-video model. Sora can generate videos up to a minute long while maintaining visual quality and adherence to the user’s prompt.
+
+
+
+  </p>
+</div>
   </div>
   )
 }
