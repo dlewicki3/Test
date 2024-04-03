fb0ddbf82788359e10685081162dffc4f82aea68
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-03 21:33:52 +0100

    ggg

--- src/component/O2.jsx
+++ src/component/O2.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./O2.css";
+
 const O2 = () => {
   return (
   <div className="o2">
 <div className="modal-bg">
-  <div className="pin"></div>
+  <div className="pin">
+<div className="kwa">Capabilities</div>
+<div className="kwa">Safety</div>
+<div className="kwa">Research</div>
+  </div>
 </div>
   </div>
   )
