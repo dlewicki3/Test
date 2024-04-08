32c596fdce6bd24c3a823cc22d741f22cf2711ba
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-08 18:52:00 +0100

    vvvvv

--- src/component/O3.jsx
+++ src/component/O3.jsx
@@ -1,28 +1,11 @@
 import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
+
 import "./O3.css";
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 const O3 = () => {
 
-  useEffect(() => {
-    gsap.fromTo(".anime", {
-      opacity: 1,
-    }, {
-      opacity: 0,
-      y: 0, // Przywrócenie pozycji pojawionego elementu
-      scrollTrigger: {
-        trigger: ".k3",
-        duration: 2, // Zwiększenie czasu trwania animacji
-        start: "200px top", // Animacja rozpocznie się, gdy górna krawędź .k3 będzie w centrum widoku
-        end: "60%", // Animacja zakończy się, gdy dolna krawędź .k3 będzie w centrum widoku
-        scrub: true,
-        markers: true,
-      }
-    });
-  }, []);
 
   return (
     <div className="o3">
