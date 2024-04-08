efd4135aa2f6ff3ae781d2e5e9d9b1b9a586339f
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-08 15:46:15 +0100

    lll

--- src/component/O2.jsx
+++ src/component/O2.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './O2.css';
-import Film2 from './Film2.mp4'; // Poprawiona ścieżka
 
 const O2 = () => {
   return (
@@ -21,11 +20,43 @@ const O2 = () => {
         </p>
       </div>
       <div className="for-vid">
-        <video autoPlay muted loop className='video'  >
-          <source src={Film2} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+       </div>
+
+       <div className="na-txt2">
+       <p className='small-txt2'>
+       <b>Today</b>
+       <br></br>
+       <br></br> Sora is becoming available to red teamers to assess critical areas for harms or risks. We are also granting access to a number of visual artists, designers, and filmmakers to gain feedback on how to advance the model to be most helpful for creative professionals.
+<br></br>
+<br></br>
+We’re sharing our research progress early to start working with and getting feedback from people outside of OpenAI and to give the public a sense of what AI capabilities are on the horizon.
+        </p>
+       </div>
+       <div className="for-vid2"></div>
+
+
+
+       <div className="na-txt3">
+       <p className='small-txt2'>
+       <b>Safety</b>
+       <br></br>
+       <br></br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ad pariatur deserunt quam id eveniet, consequuntur, voluptatem sed unde aspernatur atque commodi quo. Distinctio veniam inventore, molestiae quasi necessitatibus eum aliquid perferendis totam, aspernatur 
+<br></br>
+<br></br>
+laudantium aliquam, asperiores hic perspiciatis iste quam? Nulla maxime incidunt dolorem placeat aut. Perferendis iure sint corrupti illum mollitia repellendus molestiae soluta quidem vel fugit! Ducimus, qui! Natus eius, inventore consequatur architecto esse suscipit eveniet beatae itaque molestias maxime quos tenetur iure pariatur 
+<br></br>
+<br></br>
+debitis corporis eum placeat a rerum asperiores voluptatum sed. Quisquam incidunt aperiam recusandae quidem culpa dolor voluptatem dolore soluta reiciendis! Modi perspiciatis id nobis obcaecati adipisci impedit maxime, aliquid itaque earum cupiditate dignissimos recusandae atque voluptas ad. Illum molestias voluptate quam voluptas quidem quas culpa facere sit, rerum asperiores facilis ut voluptates 
+<br></br>
+<br></br>
+ratione quod dignissimos fugit vel aliquid in ab aperiam doloremque consequuntur voluptatibus! Nisi excepturi, ea earum quia est alias temporibus minus.
+<br></br>
+<br></br>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro illo possimus, eligendi nam sunt doloribus sint sapiente quod odit sequi tempore commodi harum animi excepturi neque deleniti modi hic deserunt! Ratione deserunt earum assumenda quos ex nostrum repellendus facere odio expedita totam illo laudantium, non labore eos impedit vel, incidunt pariatur qui repellat delectus. Ullam dolor voluptate cum illum maxime!
+        </p>
+       </div>
+       <div className="for-vid3"></div>
     </div>
   );
 }
