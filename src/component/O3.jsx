475b00f22b221ecee2a52876ebf5769a3d45eba9
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-10 15:14:31 +0100

    ffff

--- src/component/O3.jsx
+++ src/component/O3.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React from 'react';
 import "./O3.css";
+import {motion, useInView, useScroll, useTransform,} from "framer-motion";
 
 const O3 = () => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    useEffect(() => {
-        gsap.fromTo(".anime", {
-            opacity: 1,
-      
-        }, {
-            opacity: 0,
-  
-            scrollTrigger: {
-                trigger: ".o3",
-                start: "top center",
-                end: "60% center",
-                scrub: true,
-            }
-        });
-    }, []);
-
     return (
         <div className="o3">
-            <div className="anime">SORA</div>
+            <motion.div className="anime"
+            initial={{opacity:1}}
+            whileInView={{opacity:0.1}}
+            viewport={{margin:"-300px"}}
+
+
+            >SORA
+            
+            </motion.div>
         </div>
     );
 }
